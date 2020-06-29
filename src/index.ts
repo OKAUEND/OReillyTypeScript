d91f41d257a67ad65e3140c6c20f4c0fc5a45a90
OKAUEND <def1f23sbb@gmail.com>
2020-06-30 07:52:08 +0900

    ADD - 型システムの学習までをコミット

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// console.log("Hello world!");
+console.log("Hello world!");
 
 const a = 1 + 2;
 const b = a + 3;
@@ -7,3 +7,21 @@ const c = {
   banana: b
 };
 const d = c.apple * 4;
+
+const as: readonly number[] = [1, 2, 3];
+const bs: readonly number[] = as.concat(4);
+const three = bs[2];
+// as[4] = 5;
+// as.push(6);
+
+enum Language {
+  English = 0,
+  Spanish = 1,
+  Russian = 2
+}
+
+const myFirstLang = Language.Russian;
+const mySedLang = Language["English"];
+
+const h = null;
+console.log(h);
