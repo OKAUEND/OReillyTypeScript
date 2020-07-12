3d2768794f8a0b05522249cccbfdb092c1a4a891
OKAUEND <def1f23sbb@gmail.com>
2020-07-12 17:15:08 +0900

    ADD - 本日の勉強内容をコミット

--- src/ClassAndInterface.ts
+++ src/ClassAndInterface.ts
@@ -46,3 +46,125 @@ class Bishop extends Piece {}
 class Knight extends Piece {}
 class Rook extends Piece {}
 class Pawn extends Piece {}
+
+const set = new Set();
+set.add(1).add(2).add(3);
+set.has(2);
+set.has(4);
+
+class Set {
+  has(value: number): boolean {}
+  add(value: number): Set {}
+}
+
+class MutableSet extends Set {
+  delete(value: number): boolean {}
+  add(value: number): MutableSet {}
+}
+
+class Set2 {
+  has(value: number): boolean {}
+  add(value: number): this {}
+}
+
+class MutableSet2 extends Set2 {
+  delete(value: number): boolean {}
+}
+
+type Sushi = {
+  calories: number;
+  salty: boolean;
+  tasty: boolean;
+};
+
+interface Sushi2 {
+  calories: number;
+  salty: boolean;
+  tasty: boolean;
+}
+
+interface Food {
+  calories: number;
+  salty: boolean;
+}
+
+//Foodを継承してSushiインターフェイスを作成し、おいしさのプロパティを追加
+interface Sushi3 extends Food {
+  salty: boolean;
+}
+
+interface Cake extends Food {
+  sweet: boolean;
+}
+
+interface A {
+  good(x: number): string;
+  bad(x: number): string;
+}
+
+type B = A & {
+  good(x: string | number): string;
+  bad(x: string): string;
+};
+
+// interface User {
+//   name: string;
+// }
+
+// interface User {
+//   age: number;
+// }
+
+// const a: User = {
+//   name: "吉田直樹",
+//   age: 47
+// };
+
+// type User1 = {
+//   name: string;
+// };
+
+// type User1 = {
+//   age: number;
+// };
+
+// interface User {
+//   age: number;
+// }
+
+// interface User {
+//   age: string;
+// }
+
+//インターフェイスでジェネリックを宣言し、尚且2つをマージさせる場合は、ジェネリックの同じ方法(型もしくは型エイリアスなどで)で宣言しないといけない
+interface User<Age extends string> {
+  age: Age;
+}
+
+interface User<Age extends number> {
+  age: Age;
+}
+
+interface Animal {
+  readonly name: string;
+  eat(food: string): void;
+  sleep(hours: number): void;
+}
+
+class Cat implements Animal {
+  eat(food: string) {
+    console.log("Ate some", food, ",Mmm!!");
+  }
+
+  sleep(hours: number) {
+    console.info("Sleep for ", hours, "hours");
+  }
+}
+
+interface Feline {
+  meow(): void;
+}
+
+class Cat2 implements Animal, Feline {
+  name = "YOSHIDA";
+}
